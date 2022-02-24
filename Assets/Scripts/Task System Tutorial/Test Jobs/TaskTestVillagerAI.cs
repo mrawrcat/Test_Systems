7762abd23f99e4d9ec7ee89f1ede99dd3825a177
mrawrcat <wdan13@gmail.com>
2022-02-24 17:46:29 -0500

    directly made hobo into villager

--- Assets/Scripts/Task System Tutorial/Test Jobs/TaskTestVillagerAI.cs
+++ Assets/Scripts/Task System Tutorial/Test Jobs/TaskTestVillagerAI.cs
@@ -85,6 +85,10 @@ public class TaskTestVillagerAI : MonoBehaviour
     {
         state = State.TemporaryDontWaitNextTask;
     }
+    public void SetBackToWaiting()
+    {
+        state = State.WaitingForNextTask;
+    }
 
     public void DoSavedTask()
     {
